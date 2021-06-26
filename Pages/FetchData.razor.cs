30ef7a5eb871cda75baaaf6fcc044ab52cb47922
Veres-Lakos Zsombor <vl.zsombor@gmail.com>
2021-06-26 22:21:45 +0200

    code behind

---
+++ Pages/FetchData.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherServer.Data;
+
+namespace WeatherServer.Pages
+{
+    public partial class FetchData
+    {
+
+        [Inject]
+        WeatherForecastService ForecastService { get; set; }
+
+        private WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
